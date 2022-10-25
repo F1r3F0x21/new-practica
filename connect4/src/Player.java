837aefa0e6566c513529399e92ef743e52d3b18e
F1r3F0x21 <fransanchezdelaparra@gmail.com>
2022-10-25 10:01:53 +0200

    master

--- connect4/src/Player.java
+++ connect4/src/Player.java
@@ -1,14 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
 
- 
- public class Player {
+public class Player {
 
     private Color color;
     private Board board;
     private int putTokens;
 
-    Player(Color color, Board board){
+    Player(Color color, Board board) {
         assert !color.isNull();
         assert board != null;
 
@@ -16,41 +15,42 @@ import java.util.List;
         this.board = board;
         this.putTokens = 0;
     }
+
     void putToken() {
-         Coordinate coordinate;
-         Error error;
-         do {
-             coordinate = this.getCoordinate(Message.ENTER_COORDINATE_TO_PUT);
-             error = this.getPutTokenError(coordinate);
-         } while (!error.isNull());
-         this.board.putToken(coordinate, this.color);
-         this.putTokens++;
-     }
-     private Error getPutTokenError(Coordinate coordinate) {
-         assert coordinate != null;
-
-         Error error = Error.NULL;
-         if (!this.board.isEmpty(coordinate)) {
-             error = Error.NOT_EMPTY;
-         }
-         error.writeln();
-         return error;
-     }
-     Coordinate getCoordinate(Message message){
-         assert message != null;
-
-         Coordinate coordinate;
-         coordinate.read(message.toString());
-         return coordinate;
+        Coordinate coordinate;
+        Error error;
+        do {
+            coordinate = this.getCoordinate(Message.ENTER_COORDINATE_TO_PUT);
+            error = this.getPutTokenError(coordinate);
+        } while (!error.isNull());
+        this.board.putToken(coordinate, this.color);
+        this.putTokens++;
     }
-     void writeWinner() {
-         Message.PLAYER_WIN.writeln(this.color.name());
-     }
 
+    private Error getPutTokenError(Coordinate coordinate) {
+        assert coordinate != null;
 
+        Error error = Error.NULL;
+        if (!this.board.isEmpty(coordinate)) {
+            error = Error.NOT_EMPTY;
+        }
+        error.writeln();
+        return error;
+    }
+
+    Coordinate getCoordinate(Message message) {
+        assert message != null;
+
+        Coordinate coordinate;
+        coordinate.read(message.toString());
+        return coordinate;
+    }
+
+    void writeWinner() {
+        Message.PLAYER_WIN.writeln(this.color.name());
+    }
 
 }
-
 
 // CAMBIO DE PRUEBA
 
